b966c5fb1d15fac129fc3295c275f71558222a8f
tawfiqealahi <tawfiqealahi99@gmail.com>
2021-10-18 11:50:02 -0500

    2nd t commit

---
+++ src/Components/Header/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Navbar.css';
+import {BrowserRouter as Router, Route,Link} from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <>
+          <Router>
+              </Router>  
+            
+        </>
+    );
+};
+
+export default Navbar;
---
+++ src/Components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './Header/Navbar/Navbar';
+import './Home.css';
+
+const Home = () => {
+    return (
+        <div>
+    <Navbar></Navbar> 
+               
+        </div>
+    );
+};
+
+export default Home; 
