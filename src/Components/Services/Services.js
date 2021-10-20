5398a7d30eebb02b1475b3505ba6a43de8ee197a
tawfiqealahi <tawfiqealahi99@gmail.com>
2021-10-20 12:58:16 -0500

    firsst commijt

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -1,4 +1,4 @@
-import "./AllCourseDetails.css";
+import "./Services.css";
 import React, { useEffect, useState } from 'react';
 import {Button, Image, } from 'react-bootstrap';
 import { Link } from "react-router-dom";
@@ -7,7 +7,7 @@ const AllCourseDetails = () => {
   const [courses, setCourses]= useState([]) ;
   const [searchCourse , setsearchCourse]=useState("");
   useEffect(()=>{
-    fetch('./education.json'
+    fetch('./services.json'
     )
     .then(res=>res.json())
     .then(data=>setCourses(data))
@@ -23,8 +23,8 @@ const AllCourseDetails = () => {
   return (
     <div className="mx-5">
       <div >
-      <h2>Our All Services</h2>
-      <div className="search-box">
+      <h2 className="text-center">Our All Services</h2>
+      <div className="search-box text-center">
         <input onChange={handleOnChange} className="p-1" type="text" placeholder="Enter course name"/>
         <Button className="p-1 ms-1 course-container" variant="danger">Search</Button>
       </div>
@@ -32,14 +32,14 @@ const AllCourseDetails = () => {
       <div className="row">
         {
           courses.map(course=> 
-          <div className="col-md-3 ">
+          <div className="col-md-4 ">
             <div className="card mb-2">
               <div className="logo-image">
               <Image className="w-25" src={course.image} rounded />
-              <h6>Subject: {course.subject}</h6>
-              <h6>Course Teacher: {course.teacher}</h6>
-              <p>Number of Class: {course.totalClass}</p>
-              <p>Enroll Fee: {course.price}</p>
+              <h6>Subject: {course.specialist}</h6>
+              <h6>Course Teacher: {course.doctor}</h6>
+              {/* <p>Number of Class: {course.totalClass}</p> */}
+              <p>Enroll Fee: {course.fees}</p>
               {/* dynamic routing */}
               <Link to={`/details/${course.id}`}>
               <Button variant="danger">Enroll now</Button>
