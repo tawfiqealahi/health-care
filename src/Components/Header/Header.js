86a000836ccccc70395f7bc687b5404608bf847e
tawfiqealahi <tawfiqealahi99@gmail.com>
2021-11-19 10:53:18 -0600

    etaafaaaaaaae

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -11,6 +11,7 @@ const Header = () => {
                 <div className="container">
                     <Link className="navbar-brand fw-bolder " to="/home">SHASTHYA</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        
                     <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
